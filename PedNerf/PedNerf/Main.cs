6cf0bb1ac81d312eb27bb8b7cc79cd7a37ece7f6
TheBrunoCA <69942183+TheBrunoCA@users.noreply.github.com>
2021-07-18 08:34:48 -0300

    Inicio Versão 0.1a

---
+++ PedNerf/PedNerf/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+
+namespace PedNerf
+{
+    public class Main : Script
+    {
+        public static readonly string modName = "PedNerf";
+
+        public static bool valueEnableHealthChanges
+            , valueEnableAccuracyChanges
+            , valueEnableDebugMode;
+
+        public static Keys keyOpenMenu;
+
+        private readonly string pathToSettings = @"scripts\"+modName+@"\Settings.ini";
+
+        ScriptSettings settings;
+
+        private readonly string catGeneral = "General"
+            , catValues = "Values"
+            , catKeys = "Keys";
+
+        private readonly string descEnableHealthChanges = "Enable_Health_Changes"
+            , descEnableAccuracyChanges = "Enable_Accuracy_Changes"
+            , descEnableDebugMode = "Enable_Debug_Mode"
+            , descHealthValue = "Ped_Health_Value"
+            , descAccuracyValue = "Ped_Accuracy_Value"
+            , descUpdateRadius = "Mod_Update_Radius"
+            , descOpenMenu = "Open_Menu_Key";
+        public static int pedHealth, pedAccuracy, updateRadius;
+
+        public Main()
+        {
+            Tick += Main_Tick;
+            KeyUp += Main_KeyUp;
+            settings = ScriptSettings.Load(pathToSettings);
+            valueEnableHealthChanges = settings.GetValue<bool>(catGeneral, descEnableHealthChanges, true);
+            valueEnableAccuracyChanges = settings.GetValue<bool>(catGeneral, descEnableAccuracyChanges, true);
+            valueEnableDebugMode = settings.GetValue<bool>(catGeneral, descEnableDebugMode, false);
+            updateRadius = settings.GetValue<int>(catGeneral, descUpdateRadius, 100);
+            pedHealth = settings.GetValue<int>(catValues, descHealthValue, 150);
+            pedAccuracy = settings.GetValue<int>(catValues, descAccuracyValue, 10);
+            keyOpenMenu = settings.GetValue<Keys>(catKeys, descOpenMenu, Keys.F9);
+
+            Menu menu = new Menu();
+
+        }
+
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Main_Tick(object sender, EventArgs e)
+        {
+            if ((valueEnableHealthChanges || valueEnableAccuracyChanges) && isValid())
+            {
+                Ped playerPed = Game.Player.Character;
+                Ped[] peds = World.GetNearbyPeds(playerPed, updateRadius);
+                foreach (var p in peds)
+                {
+                    if(p.MaxHealth>pedHealth&&valueEnableHealthChanges)
+                    {
+                        p.MaxHealth = pedHealth;
+                    }
+                    if(p.Accuracy>pedAccuracy&&valueEnableAccuracyChanges)
+                    {
+                        p.Accuracy = pedAccuracy;
+                    }
+                }
+            }
+        }
+
+        public static bool isValid()
+        {
+            return Game.Player.Character.IsAlive && !Game.IsLoading
+                            && !Game.IsPaused;
+        }
+    }
+}
---
+++ PedNerf/PedNerf/Menu.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LemonUI;
+using LemonUI.Menus;
+using LemonUI.Elements;
+using GTA;
+
+namespace PedNerf
+{
+    class Menu : Script
+    {
+        private readonly ObjectPool pool = new ObjectPool();
+        private readonly NativeMenu menu = new NativeMenu(Main.modName, "The configuration menu")
+        {
+            ItemCount = CountVisibility.Always
+        };
+        private readonly NativeCheckboxItem enableHealthChange =
+            new NativeCheckboxItem("Enable Health Change", "Enable to apply the changes of health to Peds in range");
+        private readonly LemonUI.Menus.NativeSliderItem healthValue =
+            new NativeSliderItem("New Peds Health", "The Peds new health value", 500, Main.pedHealth);
+        private readonly NativeCheckboxItem enableAccuracyChange =
+            new NativeCheckboxItem("Enable Accuracy Change", "Enable to apply the changes of accuracy to Peds in range");
+        private readonly NativeSliderItem accuracyValue =
+            new NativeSliderItem("New Peds Accuracy", "The higher, the more likely to hit", 100, Main.pedAccuracy);
+        private readonly NativeSliderItem updateRadius =
+            new NativeSliderItem("Update Radius", "The max distance from the player the changes will be made to the ped");
+        private readonly NativeCheckboxItem enableDebugMode =
+            new NativeCheckboxItem("Debug Mode", "Developer debug tool", false);
+
+
+        public Menu()
+        {
+            Tick += Menu_Tick;
+            KeyUp += Menu_KeyUp;
+
+            enableHealthChange.CheckboxChanged += EnableHealthChange_CheckboxChanged;
+            enableAccuracyChange.CheckboxChanged += EnableAccuracyChange_CheckboxChanged;
+            healthValue.ValueChanged += HealthValue_ValueChanged;
+            accuracyValue.ValueChanged += AccuracyValue_ValueChanged;
+            updateRadius.ValueChanged += UpdateRadius_ValueChanged;
+            enableDebugMode.CheckboxChanged += EnableDebugMode_CheckboxChanged;
+
+            pool.Add(menu);
+            menu.Add(enableHealthChange);
+            menu.Add(healthValue);
+            menu.Add(enableAccuracyChange);
+            menu.Add(accuracyValue);
+            menu.Add(enableDebugMode);
+        }
+
+        private void UpdateRadius_ValueChanged(object sender, EventArgs e)
+        {
+            Main.updateRadius = updateRadius.Value;
+        }
+
+        private void EnableDebugMode_CheckboxChanged(object sender, EventArgs e)
+        {
+            Main.valueEnableDebugMode = enableDebugMode.Checked;
+        }
+
+        private void AccuracyValue_ValueChanged(object sender, EventArgs e)
+        {
+            Main.pedAccuracy = accuracyValue.Value;
+        }
+
+        private void HealthValue_ValueChanged(object sender, EventArgs e)
+        {
+            Main.pedHealth = healthValue.Value;
+        }
+
+        private void EnableAccuracyChange_CheckboxChanged(object sender, EventArgs e)
+        {
+            Main.valueEnableAccuracyChanges = enableAccuracyChange.Checked;
+            accuracyValue.Enabled = enableAccuracyChange.Checked;
+        }
+
+        private void EnableHealthChange_CheckboxChanged(object sender, EventArgs e)
+        {
+            Main.valueEnableHealthChanges = enableHealthChange.Checked;
+            healthValue.Enabled = enableHealthChange.Checked;
+        }
+
+        private void Menu_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if(e.KeyCode.Equals(Main.keyOpenMenu)&&Main.isValid())
+            {
+                if (!menu.Visible)
+                    menu.Visible = true;
+                else
+                    menu.Visible = false;
+            }
+        }
+
+        private void Menu_Tick(object sender, EventArgs e)
+        {
+            pool.Process();
+        }
+    }
+}
